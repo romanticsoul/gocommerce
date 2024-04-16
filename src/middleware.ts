eae4955143c4fbc87b43c97322985dc531789642
Ainur Yarulin <yarulinainur@gmail.com>
2024-04-16 17:32:35 +0700

    fix middleware v11

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,27 @@
 import { NextResponse } from 'next/server'
 import { auth } from './auth'
 
-// export const runtime = 'experimental-edge'
+export const runtime = 'experimental-edge'
 export const config = {
-  // matcher: ['/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)'],
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 }
 
 export default auth(req => {
   const url = req.nextUrl
-
-  // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
   const hostname = req.headers.get('host')!
-  const subdomain = hostname.split('.')[0]
+  const subdomain =
+    (hostname.match(/^(.*?)\.(?:[^.]+\.[^.]+)$/) || [])[1] || null
 
   const searchParams = url.searchParams.toString()
-  // Get the pathname of the request (e.g. /, /about, /blog/first-post)
   const path = `${url.pathname}${
     searchParams.length > 0 ? `?${searchParams}` : ''
   }`
 
-  if (subdomain == `auth`) {
+  if (!subdomain) {
+    return NextResponse.rewrite(new URL(`/home${path}`, req.url))
+  }
+
+  if (subdomain === 'auth') {
     const session = req.auth
     if (!session) {
       return NextResponse.rewrite(new URL(`/auth${path}`, req.url))
@@ -29,31 +30,5 @@ export default auth(req => {
     }
   }
 
-  // rewrite root application to `/home` folder
-  if (subdomain === 'gocommerce') {
-    return NextResponse.rewrite(new URL(`/home${path}`, req.url))
-  }
-
-  // rewrite everything else to `/[domain]/[slug] dynamic route
   return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url))
 })
-
-// export default auth(req => {
-//   const url = req.nextUrl
-
-//   const hostname = req.headers.get('host')!
-//   const subdomain = hostname.replace(`${url.host}`, '').replace('.', '') || null
-
-//   const searchParams = req.nextUrl.searchParams.toString()
-//   const path = `${url.pathname}${
-//     searchParams.length > 0 ? `?${searchParams}` : ''
-//   }`
-
-//   if (subdomain === 'auth') {
-//     return NextResponse.rewrite(new URL(`/auth${path}`, req.url))
-//   }
-//   if (subdomain === null) {
-//     return NextResponse.rewrite(new URL(`/home${path}`, req.url))
-//   }
-//   return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url))
-// })
